7a7b3339224c4575df63e2b0ef0b0a604e4f8318
Jihyun-Jang <jhjang742@gmail.com>
2021-01-10 23:26:46 +0000

    write resolve function

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,15 @@
 const graphql = require('graphql');
+const _=require('lodash');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+
+// dummy data
+const movies = [
+    { name: 'The Thing', genre: 'Science fiction horror', id: '1'},
+    { name: 'Train to Busan', genre: 'Action horror', id: '2'},
+    { name: 'The Lord of the Rings', genre: 'Fantasy', id: '3'}
+];
 
 const MovieType = new GraphQLObjectType({
     name: 'Movie',
@@ -19,6 +28,7 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLString }},
             resolve(parent, args){
                 // code to get data from db/ other source
+                return _find(movies, { id: args.id });
             }
         }
     }
