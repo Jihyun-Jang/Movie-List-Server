b29cde85b41299b1ab4df82ed7fa5158c5e4a9ae
Jihyun-Jang <jhjang742@gmail.com>
2021-01-11 00:24:57 +0000

    Add director field to the RootQuery

--- schema/schema.js
+++ schema/schema.js
@@ -12,7 +12,7 @@ const movies = [
 ];
 
 
-const director = [
+const directors = [
     { name: 'Bong Joon-ho', nationality: 'South Korean', birthYear: 1969, id: '1'},
     { name: 'John Carpenter', nationality: 'American', birthYear: 1948, id: '2'},
     { name: 'Peter Jackson', nationality: 'New Zealand', birthYear: 1961, id: '3'}
@@ -49,6 +49,13 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent, args){
                 // code to get data from db/ other source
                 return _.find(movies, { id: args.id });
+            }
+        },
+        director: {
+            type: DirectorType,
+            args: { id: { type: GraphQLID }},
+            resolve(parent, args){
+                return _.find(directors, { id: args.id });
             }
         }
     }
