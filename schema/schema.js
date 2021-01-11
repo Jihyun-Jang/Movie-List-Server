ab0a711bb820d0315605ebd75863a3a3d9aece4f
Jihyun-Jang <jhjang742@gmail.com>
2021-01-11 00:12:48 +0000

    Add Director type

--- schema/schema.js
+++ schema/schema.js
@@ -1,34 +1,54 @@
 const graphql = require('graphql');
-const _=require('lodash');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
 
 
 // dummy data
 const movies = [
     { name: 'The Thing', genre: 'Science fiction horror', id: '1'},
-    { name: 'Train to Busan', genre: 'Action horror', id: '2'},
+    { name: 'Parasite', genre: 'Comedy thriller', id: '2'},
     { name: 'The Lord of the Rings', genre: 'Fantasy', id: '3'}
 ];
+
+
+const director = [
+    { name: 'Bong Joon-ho', nationality: 'South Korean', birthYear: 1969, id: '1'},
+    { name: 'John Carpenter', nationality: 'American', birthYear: 1948, id: '2'},
+    { name: 'Peter Jackson', nationality: 'New Zealand', birthYear: 1961, id: '3'}
+]
+
 
 const MovieType = new GraphQLObjectType({
     name: 'Movie',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
     })
 });
+
+
+const DirectorType = new GraphQLObjectType({
+    name: 'Director',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        nationality: { type: GraphQLString },
+        birthYear: { type: GraphQLInt }
+    })
+});
+
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
         movie: {
             type: MovieType,
-            args: { id: { type: GraphQLString }},
+            args: { id: { type: GraphQLID }},
             resolve(parent, args){
                 // code to get data from db/ other source
-                return _find(movies, { id: args.id });
+                return _.find(movies, { id: args.id });
             }
         }
     }
