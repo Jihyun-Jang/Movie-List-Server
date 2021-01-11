9e169fb883b8df5c5c899c3aa1b0ffffa2dddd2d
Jihyun-Jang <jhjang742@gmail.com>
2021-01-11 23:51:00 +0000

    Add movies and directors fields to the RootQuery

--- schema/schema.js
+++ schema/schema.js
@@ -6,12 +6,12 @@ const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, 
 
 // dummy data
 const movies = [
-    { name: 'The Thing', genre: 'Sci-Fi horror', id: '1', directorId: '2'},
-    { name: 'Parasite', genre: 'Comedy thriller', id: '2', directorId: '1'},
-    { name: 'The Lord of the Rings', genre: 'Fantasy', id: '3', directorId: '3'},
-    { name: 'Snowpiercer', genre: 'Sci-Fi drama', id: '4', directorId: '1'},
-    { name: 'The Hobbit', genre: 'Fantasy', id: '5', directorId: '3'},
-    { name: 'Okja', genre: 'Action adventure', id: '6', directorId: '1'}
+    { name: 'The Thing', genre: 'Sci-Fi horror', id: '1', year: 1982, directorId: '2'},
+    { name: 'Parasite', genre: 'Comedy thriller', id: '2', year: 2019, directorId: '1'},
+    { name: 'The Lord of the Rings', genre: 'Fantasy', id: '3', year: 2001,directorId: '3'},
+    { name: 'Snowpiercer', genre: 'Sci-Fi drama', id: '4', year: 2013, directorId: '1'},
+    { name: 'The Hobbit', genre: 'Fantasy', id: '5', year: 2012, directorId: '3'},
+    { name: 'Okja', genre: 'Action adventure', id: '6', year: 2017, directorId: '1'}
 ];
 
 
@@ -28,10 +28,10 @@ const MovieType = new GraphQLObjectType({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
+        year: { type: GraphQLInt },
         director: {
             type: DirectorType,
-            resolve(parent, args){
-                console.log(parent);
+            resolve(parent, args){                
                 return _.find(directors, { id: parent.directorId })
             }
         }
@@ -72,6 +72,18 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLID }},
             resolve(parent, args){
                 return _.find(directors, { id: args.id });
+            }
+        },
+        movies: {
+            type: new GraphQLList(MovieType),
+            resolve(parent, args){
+                return movies;
+            }
+        },
+        directors: {
+            type: new GraphQLList(DirectorType),
+            resolve(parent, args){
+                return directors;
             }
         }
     }
