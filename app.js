49f39f205c719783cdf6f0fa03cc72e719b0412c
Jihyun-Jang <jhjang742@gmail.com>
2021-01-12 18:36:56 +0000

    Set up database with MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const app = express();
+
+dotenv.config();
+
+const url = process.env.MONGOLAB_URI;
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("Connected to MongoDB Atlas"))
+.catch(err => console.log("Error: ", err.message));
 
 app.use('/graphql', graphqlHTTP({
     schema, // schema: schema
